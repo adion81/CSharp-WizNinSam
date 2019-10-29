1dca9aeab76fbeb87107c36ff4d8ff2b194d6950
Adrien Dion <adion81@gmail.com>
2019-10-29 12:36:57 -0500

    Finished Wizard class

--- Models/Wizard.cs
+++ Models/Wizard.cs
@@ -1,7 +1,31 @@
+using System;
 namespace WizNinSam.Models
 {
-    public class Wizard
+    public class Wizard : Human
     {
-        
+        public Wizard(string name) : base(name)
+        {
+            health = 50;
+            Intelligence = 25;
+        }
+
+        public override int Attack(Human target)
+        {
+            int dmg = Intelligence * 5;
+            target.Health -= dmg;
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine($"{Name} casted a magical spell on {target.Name} for {dmg} health!");
+            Console.ResetColor();
+            return target.Health;
+        }
+        public void Heal(Human target)
+        {
+            int heal = Intelligence * 10;
+            target.Health += heal;
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine($"{Name} casted a magical spell on {target.Name} for {heal} health!");
+            Console.ResetColor();
+
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -9,11 +9,14 @@ namespace WizNinSam
         {
             Console.WriteLine("WizNinSam");
             Human Adrien = new Human("Adrien");
-            Samurai me = new Samurai("Benny Bob");
-            me.Attack(Adrien);
-            me.Meditate();
+            Samurai Benny = new Samurai("Benny Bob");
+            Benny.Attack(Adrien);
+            Benny.Meditate();
             Ninja Nibbles = new Ninja("Mr. Nibbles");
-            Nibbles.Attack(me);
+            Nibbles.Attack(Benny);
+            Wizard Walter = new Wizard("Walter Morgan");
+            Walter.Attack(Benny);
+            Walter.Heal(Adrien);
         }
     }
 }
